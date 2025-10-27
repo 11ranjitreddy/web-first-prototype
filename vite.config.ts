06fb521d8f085eb848c0c92a19187a1f07a04ad3
ranjit <ranjithredd8479@gmail.com>
2025-10-27 16:02:33 +0530

    base name is removed causes some errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/OtpService/",
+  
   server: {
     host: "::",
     port: 1111,
